9cfb78713aacc49f39c03e1b80939ec3457298b4
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-03-23 23:17:36 +0200

    create states and create use cases

--- app/src/main/java/com/robosh/basestartapplication/home/presenter/HomeViewModel.kt
+++ app/src/main/java/com/robosh/basestartapplication/home/presenter/HomeViewModel.kt
@@ -3,18 +3,20 @@ package com.robosh.basestartapplication.home.presenter
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.robosh.basestartapplication.home.usecase.GetMoviesUseCase
+import com.robosh.basestartapplication.home.usecase.GetMoviesUseCaseImpl
 import com.robosh.basestartapplication.net.repository.MovieRepository
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val movieRepository: MovieRepository) : ViewModel() {
 
+    private val getMoviesUseCase: GetMoviesUseCase = GetMoviesUseCaseImpl()
+
     fun aaaaa() {
         Log.d("TAGGERR", "asdasdas")
 
         viewModelScope.launch {
-            movieRepository.getMovieListReference().isSuccessful
-            Log.d("TAGGERR", movieRepository.getMovieListReference().message())
-            Log.d("TAGGERR", movieRepository.getMovieListReference().body().toString())
+            Log.d("TAGGERR", getMoviesUseCase.execute().toString())
         }
     }
 }
---
+++ app/src/main/java/com/robosh/basestartapplication/home/usecase/GetMoviesUseCaseImpl.kt
@@ -0,0 +1,25 @@
+package com.robosh.basestartapplication.home.usecase
+
+import com.robosh.basestartapplication.model.MovieState
+import com.robosh.basestartapplication.net.RetrofitClientInstance
+import com.robosh.basestartapplication.net.mapper.MovieMapper
+import com.robosh.basestartapplication.net.repository.MovieRepository
+import com.robosh.basestartapplication.net.repository.MovieRepositoryImpl
+
+class GetMoviesUseCaseImpl : GetMoviesUseCase {
+
+    private val movieRepository: MovieRepository =
+        MovieRepositoryImpl(RetrofitClientInstance.retrofitInstance)
+    private val movieMapper = MovieMapper()
+
+    override suspend fun execute(): MovieState {
+        val movieListResponse = movieRepository.getMovieListReference()
+        return if (movieListResponse.isSuccessful) {
+            MovieState.DataListState(
+                movieMapper.map(movieListResponse.body()?.results ?: emptyList())
+            )
+        } else {
+            MovieState.ErrorState("Some error occurred: ${movieListResponse.errorBody()}")
+        }
+    }
+}
