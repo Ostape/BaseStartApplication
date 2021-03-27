1c4c30a84731cdfb9843c1d762ecab931085ecd7
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-03-27 16:43:26 +0200

    fix alarm manager when Doze mode

--- app/src/main/java/com/robosh/basestartapplication/home/view/HomeFragment.kt
+++ app/src/main/java/com/robosh/basestartapplication/home/view/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,14 +14,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.work.OneTimeWorkRequest
 import com.robosh.basestartapplication.databinding.FragmentHomeBinding
 import com.robosh.basestartapplication.home.presenter.HomeViewModel
 import com.robosh.basestartapplication.model.Movie
 import com.robosh.basestartapplication.model.MovieEvent
 import com.robosh.basestartapplication.model.MovieState
 import com.robosh.basestartapplication.receiver.AlarmNotificationReceiver
-import com.robosh.basestartapplication.workmanager.MovieSchedulerWorkManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
@@ -74,19 +73,25 @@ class HomeFragment : Fragment(), MovieClickCallback {
 
     override fun onMovieRemindClicked(movie: Movie) {
         val instance = Calendar.getInstance()
-        instance.set(Calendar.MINUTE, instance.get(Calendar.MINUTE) + 1)
+        instance.set(Calendar.MINUTE, instance.get(Calendar.MINUTE))
+        instance.set(Calendar.SECOND, instance.get(Calendar.SECOND) + 10)
         startAlarm(instance)
     }
 
     private fun startAlarm(calendar: Calendar) {
         val manager = requireActivity().getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val myIntent: Intent
         val pendingIntent: PendingIntent
 
-        myIntent = Intent(requireContext(), AlarmNotificationReceiver::class.java)
+        val myIntent = Intent(requireContext(), AlarmNotificationReceiver::class.java)
         pendingIntent = PendingIntent.getBroadcast(requireContext(), 0, myIntent, 0)
 
         Log.d("TAGGGG", calendar.timeInMillis.toString())
-        manager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent)
+        Log.d("TAGGGG", System.currentTimeMillis().toString())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            manager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        } else {
+            manager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        }
     }
 }
