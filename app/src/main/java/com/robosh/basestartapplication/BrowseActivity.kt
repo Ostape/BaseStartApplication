a969f7c192138e507a30d12eacedd673e319284e
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-08-25 10:55:43 +0300

    fix tests

--- app/src/main/java/com/robosh/basestartapplication/BrowseActivity.kt
+++ app/src/main/java/com/robosh/basestartapplication/BrowseActivity.kt
@@ -3,11 +3,24 @@ package com.robosh.basestartapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class BrowseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse)
+        val launch = CoroutineScope(Dispatchers.Default).launch {
+
+        }
+
+        launch.cancel()
     }
+}
+
+fun main() {
+
 }
